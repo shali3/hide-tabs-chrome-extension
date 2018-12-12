cb7e62c11a66479c312c0fb4d08ed9db398ed646
ShaLi Shaltiel <shali3@gmail.com>
2018-12-12 11:02:09 +0200

    Add support for hot reloading of the content script

--- src/background.js
+++ src/background.js
@@ -1,3 +1,8 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-    console.log('Clicked in ' + tab.url );
+    console.log('Clicked in ' + tab.url);
+});
+chrome.tabs.query({ url: ["http://*/*", "https://*/*",] }, function (tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+        chrome.tabs.executeScript(tabs[i].id, { file: "contentScript.js" }, function () { });
+    }
 });
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,22 +1,74 @@
-var cover = document.createElement('div');
-cover.className = 'hideTabCover'
+(function () {
+    const cleanupEvent = new Event('cleanup');
+    const coverClassName = 'hideTabCover';
+    var cover;
+    var timer;
 
-document.body.appendChild(cover);
-var timer;
-function showTab(ev) {
-    clearTimeout(timer);
-    console.log("Showing tab on event '%s'", ev ? ev.type : 'no event')
-    cover.style.opacity = 0;
-    if (ev && ev.type == 'focus') {
-        timer = setTimeout(hideTab, 3000, { type: 'focus-timeout' });
+    function init() {
+        cleanupOldCovers();
+        cover = createCover()
+        registerEvents()
+
+        document.body.appendChild(cover)
+
     }
-}
 
-function hideTab(ev) {
-    clearTimeout(timer);
-    console.log("Hiding tab on event '%s'", ev ? ev.type : 'no event')
-    cover.style.opacity = 1;
-}
+    function cleanupOldCovers() {
+        const existingCovers = document.getElementsByClassName(coverClassName);
+        console.log('Cleaning up %d existing covers', existingCovers.length);
+        for (var i = 0; i < existingCovers.length; i++) {
+            const existingCover = existingCovers[i];
+            existingCover.dispatchEvent(cleanupEvent);
+            existingCover.parentNode.removeChild(existingCover);
+        }
+    }
+    function registerEvents() {
+        cover.addEventListener(cleanupEvent.type, onCleanup)
+        window.addEventListener('focus', showTab)
+        document.addEventListener('mouseenter', showTab)
+        document.addEventListener('mouseleave', hideTab)
+    }
 
-window.onfocus = document.onmouseenter = showTab;
-document.onmouseleave = hideTab;
+    function createCover() {
+        const cover = document.createElement('div');
+        cover.className = coverClassName
+        const style = cover.style
+        style.position = 'fixed'
+        style.zIndex = 10000
+        style.right = style.left = style.top = style.bottom = 0
+        style.background = '#2b2b2b'
+        style.transition = '0.5s'
+        style.pointerEvents = 'none'
+        style.opacity = 0
+
+        return cover;
+    }
+    function showTab(ev) {
+        clearTimeout(timer);
+        console.log("Showing tab on event '%s'", ev ? ev.type : 'no event')
+        cover.style.opacity = 0;
+        if (ev && ev.type == 'focus') {
+            timer = setTimeout(hideTab, 3000, { type: 'focus-timeout' })
+        }
+    }
+
+    function hideTab(ev) {
+        clearTimeout(timer);
+        console.log("Hiding tab on event '%s'", ev ? ev.type : 'no event')
+        cover.style.opacity = 1;
+    }
+
+    function onCleanup(event) {
+        console.log('Cleaning up old content script')
+        cover.removeEventListener(event.type, onCleanup)
+        window.removeEventListener('focus', showTab)
+        document.removeEventListener('mouseenter', showTab)
+        document.removeEventListener('mouseleave', hideTab)
+        clearTimeout(timer);
+        timer = null;
+        cover = null;
+    }
+    init();
+})()
+
+
